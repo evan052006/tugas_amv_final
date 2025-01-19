ffb3009c1e5b6b92f1944c1b2e373f5b04e62d0a
Vane <88143635+evan052006@users.noreply.github.com>
2025-01-19 13:40:57 +0700

    Made trackbars to find adjust color masks

--- scripts/Utama.py
+++ scripts/Utama.py
@@ -17,27 +17,58 @@ class Utama(object):
         self.bridge = CvBridge()
         self.current_red_count = 0
         self.current_green_count = 0
+
+        self.red_lower_left_limit = np.array([10,50,50])
+        self.red_upper_left_limit = np.array([10,255,255])
+        self.red_lower_right_limit = np.array([170,50,50])
+        self.red_upper_right_limit = np.array([180,255,255])
+
+        self.green_lower_limit = np.array([40,50,50])
+        self.green_upper_limit = np.array([80,255,255])
+
+        cv2.createTrackbar("red_trackbar", "red", 0, 179, self.red_trackbar_oncall)
+        cv2.createTrackbar("green_lower_trackbar", "red", 0, 179, self.green_lower_trackbar_oncall)
+        cv2.createTrackbar("green_upper_trackbar", "green", 0, 179, self.green_upper_trackbar_oncall)
+
+        #red_lower_left_limit = np.array([10,50,50])
+        #red_upper_left_limit = np.array([10,255,255])
+        #red_lower_right_limit = np.array([170,50,50])
+        #red_upper_right_limit = np.array([180,255,255])
+
+        #green_lower_limit = np.array([40,50,50])
+        #green_upper_limit = np.array([80,255,255])
+
+    def red_trackbar_oncall(self, val):
+        self.red_lower_left_limit = np.array([val,50,50])
+        self.red_upper_left_limit = np.array([val,255,255])
+        self.red_lower_right_limit = np.array([179-val,50,50])
+        self.red_upper_right_limit = np.array([179-val,255,255])
     
+    def green_lower_trackbar_oncall(self, val):
+        self.green_lower_limit = val
+    
+    def green_upper_trackbar_oncall(self, val):
+        self.green_upper_limit = val
+
     def callback(self, data):
         current_frame = self.bridge.imgmsg_to_cv2(data)
         hsv_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2HSV)
-
-        red_lower_left_limit = np.array([10,50,50])
-        red_upper_left_limit = np.array([10,255,255])
-        red_lower_right_limit = np.array([170,50,50])
-        red_upper_right_limit = np.array([180,255,255])
+        
         red_mask = \
-            cv2.inRange(hsv_frame, red_lower_left_limit, red_upper_left_limit) \
-            | cv2.inRange(hsv_frame, red_lower_right_limit, red_upper_right_limit)
+            cv2.inRange(hsv_frame, self.red_lower_left_limit, self.red_upper_left_limit) \
+            | cv2.inRange(hsv_frame, self.red_lower_right_limit, self.red_upper_right_limit)
         red = cv2.bitwise_and(hsv_frame, hsv_frame, mask=red_mask)
-
-        green_lower_limit = np.array([40,50,50])
-        green_upper_limit = np.array([80,255,255])
-        green_mask = cv2.inRange(hsv_frame, green_lower_limit, green_upper_limit)
+        red_grayscale = cv2.COLOR_BGR2GRAY(red)
+        
+        green_mask = cv2.inRange(hsv_frame, self.green_lower_limit, self.green_upper_limit)
         green = cv2.bitwise_and(hsv_frame, hsv_frame, mask=green_mask)
+        green_grayscale = cv2.COLOR_BGR2GRAY(green)
 
-        red_count = self.detect_count_from_grayscale(current_frame, red)
-        green_count = self.detect_count_from_grayscale(current_frame, green)
+        cv2.imshow("red", red_grayscale)
+        cv2.imshow("green", green_grayscale)
+
+        red_count = self.detect_count_from_grayscale(current_frame, red_grayscale)
+        green_count = self.detect_count_from_grayscale(current_frame, green_grayscale)
 
         self.publisher.publish(BuoyCount(red_count, green_count))
 
